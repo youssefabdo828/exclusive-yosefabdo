b0d925b099f93385feb9c5df643eb7054ecc56a1
youssefabdo828 <yosefabdo828@gmail.com>
2025-09-18 07:55:05 +0300

    changes in token prod

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -4,12 +4,15 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-    
-    const encodedToken = (await cookies()).get("next-auth.session-token")?.value;
-    
-    
-    const decodedToken = await decode({token: encodedToken , secret:process.env.AUTH_SECRET!})
-    
+
+    const cookieStore = await cookies();
+    const encodedToken =
+        cookieStore.get("next-auth.session-token")?.value ||
+        cookieStore.get("__Secure-next-auth.session-token")?.value;
+
+
+    const decodedToken = await decode({ token: encodedToken, secret: process.env.AUTH_SECRET! })
+
 
     return decodedToken!.token;
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,9 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
+const cookieName = process.env.NODE_ENV === "production" ? "__Secure-next-auth.session-token" : "next-auth.session-token" ;
 
-
-   const token =  await getToken({req:request})
+   const token =  await getToken({req:request , cookieName})
    const {pathname} = request.nextUrl;
 
 
